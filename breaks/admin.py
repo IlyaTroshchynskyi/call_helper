cf57cd50a8093c3a65e573e269b00a6b71f63f07
Illia Troshchynskyi <itroshchinskiy@rambler.ua>
2023-02-26 21:58:35 +0200

    added Replacement, ReplacementStatus, ReplacementEmployee models

--- breaks/admin.py
+++ breaks/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
+from django.contrib.admin import TabularInline
 
-from breaks.models.organizations import Organization, Group
+from breaks.models.organizations import (
+    Organization,
+    Group,
+    Replacement,
+    ReplacementStatus,
+    ReplacementEmployee,
+)
+
+
+class ReplacementEmployeeInline(TabularInline):
+    model = ReplacementEmployee
+    fields = ("employee", "status")
 
 
 @admin.register(Organization)
@@ -15,3 +27,14 @@ class OrganizationAdmin(admin.ModelAdmin):
 @admin.register(Group)
 class OrganizationAdmin(admin.ModelAdmin):
     list_display = ("id", "name", "manager", "min_active")
+
+
+@admin.register(Replacement)
+class ReplacementAdmin(admin.ModelAdmin):
+    list_display = ("id", "group", "break_start", "break_end", "break_max_duration")
+    inlines = (ReplacementEmployeeInline,)
+
+
+@admin.register(ReplacementStatus)
+class ReplacementStatusAdmin(admin.ModelAdmin):
+    list_display = ("code", "name", "sort", "is_active")
--- breaks/models/organizations.py
+++ breaks/models/organizations.py
@@ -47,3 +47,61 @@ class Group(models.Model):
         verbose_name = "Group"
         verbose_name_plural = "Groups"
         ordering = ("name",)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Replacement(models.Model):
+    group = models.ForeignKey(
+        Group, models.CASCADE, "replacements", verbose_name="Group"
+    )
+    date = models.DateField("Date of replacement")
+    break_start = models.TimeField("Start break")
+    break_end = models.TimeField("End break")
+    break_max_duration = models.PositiveSmallIntegerField("Max time of duration break")
+
+    class Meta:
+        verbose_name = "Replacement"
+        verbose_name_plural = "Replacements"
+        ordering = ("-date",)
+
+    def __str__(self):
+        return f"Replacement №{self.pk} for {self.group}"
+
+
+class ReplacementStatus(models.Model):
+    code = models.CharField("Code", max_length=16, primary_key=True)
+    name = models.CharField("Name", max_length=32)
+    sort = models.PositiveSmallIntegerField("Sorting", null=True, blank=True)
+    is_active = models.BooleanField("Activity Status", default=True)
+
+    class Meta:
+        verbose_name = "Replacement Status"
+        verbose_name_plural = "Replacements Status"
+        ordering = ("sort",)
+
+    def __str__(self):
+        return f"{self.code} for {self.name}"
+
+
+class ReplacementEmployee(models.Model):
+    employee = models.ForeignKey(
+        User, models.CASCADE, "replacements", verbose_name="Employees"
+    )
+    replacement = models.ForeignKey(
+        Replacement, models.CASCADE, "employees", verbose_name="Replacement"
+    )
+    status = models.ForeignKey(
+        ReplacementStatus,
+        models.CASCADE,
+        "replacement_employees",
+        verbose_name="Status",
+    )
+
+    class Meta:
+        verbose_name = "Replacement - Employees"
+        verbose_name_plural = "Replacements - Employees"
+
+    def __str__(self):
+        return f"Replacement {self.replacement} for {self.employee}"
